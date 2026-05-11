2798d7f44c7208efe10aa3536f2e8f17fb3629b6
Busola <busolaokemoney@gmail.com>
2026-05-11 14:22:35 +0100

    refactor: inject default MDX components into the docs page content renderer

--- apps/docs/app/docs/[[...slug]]/page.tsx
+++ apps/docs/app/docs/[[...slug]]/page.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { notFound } from 'next/navigation'
 import { DocsPage, DocsBody, DocsTitle, DocsDescription } from 'fumadocs-ui/page'
+import defaultMdxComponents from 'fumadocs-ui/mdx'
 import { source } from '@/app/source'
 
 interface Props {
@@ -19,7 +20,7 @@ export default async function Page({ params }: Props) {
       <DocsTitle>{page.data.title}</DocsTitle>
       <DocsDescription>{page.data.description}</DocsDescription>
       <DocsBody>
-        <MDX />
+        <MDX components={{ ...defaultMdxComponents }} />
       </DocsBody>
     </DocsPage>
   )
